73364583087dde4c0f6ae4048560a5174fd938a9
Mamadou Faye <mamadou.faye1@yahoo.fr>
2017-01-17 18:17:35 -0500

    first commit

---
+++ include/catalogue.php
@@ -0,0 +1,16 @@
+<?php
+require_once 'define.php';
+$page_title = 'catalogue';
+?>
+
+<?php
+require_once 'views/header.php';
+require_once 'views/aside.php';
+?>
+<main>
+    <h2>catalogue</h2>
+</main>
+<?php
+require_once 'views/footer.php';
+?>
+
---
+++ include/index.php
@@ -0,0 +1,15 @@
+<?php
+require_once 'define.php';
+$page_title = 'Accueil';
+?>
+
+<?php
+require_once 'views/header.php';
+require_once 'views/aside.php';
+?>
+<main>
+    <h2>la page acceuil</h2>
+</main>
+<?php
+require_once 'views/footer.php';
+?>
