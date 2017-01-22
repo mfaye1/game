044b7c06d1504d8468b43642cbfda876ca7375a5
Mamadou Faye <mamadou.faye1@yahoo.fr>
2017-01-21 22:12:37 -0500

    le top

--- include/views/top.php
+++ include/views/top.php
@@ -4,6 +4,6 @@
 <head>
     <meta charset="utf-8">
     <title>$page_title</title>
-    <link rel="stylesheet" href="include/main.css">
+    <link rel="stylesheet" href="css/main.css">
     <script src="script/main.js"></script>
 </head>
