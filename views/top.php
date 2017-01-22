13f73defcd60f320428b536291409b8f08ee435e
Mamadou Faye <mamadou.faye1@yahoo.fr>
2017-01-22 13:18:57 -0500

    le top avec du viewport

--- views/top.php
+++ views/top.php
@@ -2,7 +2,8 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>$page_title</title>
+    <meta name="viewport" content="width=device-width, intial-scale=1">
     <link rel="stylesheet" href="../css/main.css" />
     <script src="../script/main.js"></script>
+    <title>$page_title</title>
 </head>
