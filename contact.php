30f2e9d09671ec9f35fad67e56c95a742d10d0a7
Mamadou Faye <mamadou.faye1@yahoo.fr>
2017-01-23 21:25:02 -0500

    validation

--- contact.php
+++ contact.php
@@ -40,33 +40,41 @@ if (array_key_exists('saisi_email', $_POST)) {
     $email = filter_input(INPUT_POST, 'saisi_email', FILTER_SANITIZE_STRING);
     $email_valide = (false !== filter_var($email, FILTER_VALIDATE_EMAIL));
 }
-
+/*validation du telephone*/
 $telephone = '';
 $telephone_valide = true;
 if (array_key_exists('saisi_telephone', $_POST)) {
     $telephone = filter_input(INPUT_POST, 'saisi_telephone', FILTER_SANITIZE_STRING);
-    $telephone_valide = (1 === preg_match('/\^\\\(\?\(\[0\-9\]\{3\}\)\\\)\?\[\-\. \]\?\(\[0\-9\]\{3\}\)\[\-\. \]\?\(\[0\-9\]\{4\}\)\$ /', $telephone));
+    $telephone_valide = (1 === preg_match('#(\+[0-9]{1}\([0-9]\))?[0-9]{10}#', $telephone));
 
 }
-
+/*validation des mots de pass */
+$password = '';
+$password_valide = true;
+if (array_key_exists('saisi_password', $_POST)) {
+    $password = filter_input(INPUT_POST, 'saisi_password', FILTER_SANITIZE_STRING);
+    $password_valide = (1 === preg_match('/[A-Za-z0-9]\w{1,}/',$password));
+}
+/*laaaa confirmation*/
+$confirmation = '';
+$confirmation_valide = true;
+if (array_key_exists('saisi_confirmation', $_POST)) {
+    $confirmation = filter_input(INPUT_POST, 'saisi_password', FILTER_SANITIZE_STRING);
+    $confirmation_valide = (1 === preg_match('/[A-Za-z0-9]\w{1,}/',$confirmation));
+}
+if ($password !== $confirmation){
+    echo "<p>les deux mot de pass ne sont pas identique</p>";
+}
 
 /******/
 
-if ($reception && $nom_valide && $prenom_valide && $email_valide && $sexe_valide && $telephone_valide
-    && $ville_valide
-) {
-    // Les données de formulaire sont valides
-    header('Location:inscrire.php');
-    exit;
-}
-/**********************************************/
 $sexe = array();
 $sexe_valide = true;
 if (array_key_exists('sexe', $_POST)) {
     $sexe = $_POST['sexe'];
 }
-if ($en_reception && empty($sexe)) {
-    $villes_valide = false;
+if ($reception && empty($sexe)) {
+    $sexe_valide = false;
 }
 // Réception des ville choisis
 $ville_valide = true;
@@ -79,7 +87,15 @@ if ($reception && empty($ville)) {
     $ville_valide = false;
 }
 
+/**********************************************/
 
+if ($reception && $nom_valide && $prenom_valide && $email_valide && $sexe_valide && $telephone_valide
+    && $ville_valide && $password_valide && $confirmation_valide
+) {
+    // Les données de formulaire sont valides
+    header('Location:inscrire.php');
+    exit;
+}
 ?>
 <div id="wrapper">
     <div class="row">
@@ -89,7 +105,8 @@ if ($reception && empty($ville)) {
         <form id="form" method="post">
             <div class="<?= $nom_valide ? '' : 'invalid' ?>">
                 <label for="saisi_nom">Nom: </label>
-                <input type="text" id="saisi_nom" name="saisi_nom" placeholder="entrez votre nom" value="<?= $nom ?>">
+                <input type="text" id="saisi_nom" name="saisi_nom" placeholder="entrez votre nom"
+                       value="<?= $nom ?>">
                 <?php
                 if (!$nom_valide) {
                     echo "<p>veuillez entre un nom valide avec au moins un caractere</p>";
@@ -110,7 +127,7 @@ if ($reception && empty($ville)) {
 
             <div class="<?= $telephone_valide ? '' : 'invalid' ?>">
                 <label for="saisi_telephone">Telephone: </label>
-                <input type="text" id="saisi_telephone" name="saisi_telephone" placeholder="***-***-****"
+                <input type="text" id="saisi_telephone" name="saisi_telephone"  placeholder="***-***-****"
                        value="<?= $telephone ?>">
                 <?php
                 if (!$telephone_valide) {
@@ -121,7 +138,7 @@ if ($reception && empty($ville)) {
 
             <div class="<?= $email_valide ? '' : 'invalid' ?>">
                 <label for="saisi_email">Email: </label>
-                <input type="text" id="saisi_email" name="saisi_email" placeholder="example@emample.con"
+                <input type="text" id="saisi_email" name="saisi_email"  placeholder="example@emample.con"
                        value="<?= $email ?>">
                 <?php
                 if (!$email_valide) {
@@ -131,17 +148,30 @@ if ($reception && empty($ville)) {
             </div>
 
             <div class="">
-                <label for="">Mot De Passe: </label>
-                <input type="password" id="" name="" placeholder="" value="">
+                <label for="saisi_password">Mot De Passe: </label>
+                <input type="password" id="saisi_password" name="saisi_password"  value="<?=$password?>">
+                <?php
+                if (!$password_valide) {
+                    echo "<p>veuillez indiquer un mot de passe </p>";
+
+                }
+                ?>
             </div>
             <div>
-                <label for="">Confirmation Mot De Passe: </label>
-                <input type="password" id="" name="" placeholder="" value="">
+                <label for="saisi_confirmation">Confirmation Mot De Passe: </label>
+                <input type="password" id="saisi_confirmation" name="saisi_confirmation"  value="<?=$confirmation?>">
+                <?php
+                if (!$confirmation_valide) {
+                    echo "<p>veuillez confirmer le mot de passe</p>";
+
+                }
+                ?>
             </div>
             <div>
                 <label for="sexe">Sexe: </label>
                 Homme <input type="radio" name="sexe[]" id="sexe" value="H">
                 Femme <input type="radio" name="sexe[]" id="sexe" value="F">
+
                 <?php
                 if (!$sexe_valide) {
                     echo "<p>veuillez cochez au moins une case</p>";
@@ -156,7 +186,7 @@ if ($reception && empty($ville)) {
             </div>
             <div>
                 <label for="ville">ville: </label>
-                <select name="ville[]" id="ville" multiple="multiple">
+                <select name="ville[]" id="ville" multiple="multiple" >
                     <?php foreach ($liste_ville as $ville) {
                         $option_value = retire_accents($ville);
                         ?>
--- script/main.js
+++ script/main.js
@@ -2,16 +2,22 @@
  * Created by Mamadou Faye on 17/01/2017.
  */
 "use strict";
-const MIN_NB_CAR = 1;
-$ (function () {
-    /*brancher l<ecoute au momment du submit*/
-    var formulaire = ('#form');
-    formulaire.on('submit',valider_formulaire);
-    /*ecouter les changement*/
-    formulaire.find(':input').on('change', gerer_changement);
-    formulaire.find('select[name="ville"]').on('change', gerer_changement_ville);
-});
+var validation = document.getElementById('submit');
+// recupere les element et le span pour au cas ou sa manque
+var prenom = document.getElementById('saisi_prenom'); console.log(prenom);
+var prenom_m= document.getElementById('prenom_manquant');
+var prenom_reg = /^[a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+([-'\s][a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+)?$/;//ecrire ici le regex du prenom
+validation.addEventListener('click',f_valide);
+function f_valide(e) {
+    if (prenom.validity.valueMissing){
+        e.preventDefault();
+        prenom_m.textContent="ce champ est requis veuillez le remplir";
+        prenom_m.style.color = "red";
+    }else if (prenom_reg.test(prenom.value)==false){
+        event.preventDefault();
+        prenom_m.textContent="format non valide";
+        prenom_m.style.color = "orange"
+    }else {
 
-/*gestionnairee des evenement pour eviter la soumission en cas de besoin*/
-
-
+    }
+}
