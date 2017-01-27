9057c05066a7d75d986ac3c18cf39deb54a26afc
Mamadou Faye <mamadou.faye1@yahoo.fr>
2017-01-27 17:52:02 -0500

    connexion au menu

--- acceuil.php
+++ acceuil.php
@@ -6,44 +6,7 @@ $page_title = 'Accueil';
 <?php
 require_once 'views/top.php';
 require_once 'views/header.php';
-require_once 'user_authenticate.php'; // Code pour authentification
-define('PS_USERNAME', 'ps_username');
-
-session_start(); // Démarrage session
-
-function is_logged_in() {
-    return (array_key_exists(PS_USERNAME, $_SESSION) && (! empty($_SESSION[PS_USERNAME]))); // Par convention, l'utilisateur est connecté si la données de session username n'est pas vide
-}
-
 ?>
-<?php
-$username='';
-$password='';
-$username_valide = true;
-$password_valide = true;
-if (is_logged_in() && array_key_exists('logout_btn', $_POST)) {
-    // Déconnexion à effectuer
-    $_SESSION[PS_USERNAME] = ''; // Username vide signifie non connecté
-} else if ( ! is_logged_in()
-    && array_key_exists('login_btn', $_POST)
-    && array_key_exists('username', $_POST)
-    && array_key_exists('password', $_POST)) {
-    // Connexion à effectuer ici
-    // Filtrage et Validation
-    $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
-    $username_valide = (1===preg_match('/\w{5,}/', $username));
-    $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
-    $password_valide = (1===preg_match('/\w{6,}/', $password));
-    if ($username_valide
-        && $password_valide
-        && user_authenticate($username, $password)) {
-        // Si tout se passe bien on le connecte
-        $_SESSION[PS_USERNAME] = $username;
-    }
-}
-
-?>
-
 <?php
 /*$en_reception = array_key_exists('saisie_username', $_POST) && array_key_exists('saisie_password', $_POST);
 
@@ -163,45 +126,6 @@ if ($en_reception && $username_valide && $password_valide)
         </div>
         <div class="col-4" id="aside">
 
-            <div id="connexion">
-
-                <?php if (is_logged_in()) { ?>
-                    <h2 style="visibility: hidden">CONNEXION</h2>
-                    <marquee direction="left" scrollamount="4" onMouseOver="this.stop();" onMouseOut="this.start();">
-                        <span>Salut <?= $_SESSION[PS_USERNAME] ?> !, bienvenu a votre espace personel</span>
-                    </marquee>
-                    <form name="logout" method="post">
-                        <input type="submit" id="logout_btn" name="logout_btn" value="Déconnecter" />
-                        <div style="visibility: hidden">
-                            <a href="inscription.php">s'inscrire</a>
-                        </div>
-                    </form>
-                <?php } else { ?>
-                    <h2 style="visibility: visible">CONNEXION</h2>
-                    <form name="login" method="post">
-                        <div>
-                        <label for="username">Identifiant:</label>
-                        <input type="text" name="username" id="username"
-                               value="<?= array_key_exists('username',$_POST) ? $_POST['username'] : '' ?>"
-                        />
-                        </div>
-                        <div>
-                        <label for="password">Mot de passe:</label>
-                        <input type="password" name="password" id="password" value="" />
-                        </div>
-                        <?php // Afficher un message dans le cas ou l'authentification n'a pas réussi
-                        if (array_key_exists('login_btn',$_POST)) { ?>
-                            <p>Le pseudo et le mot de passe fournis ne concordent pas.</p>
-                        <?php } ?>
-                        <input type="submit" id="login_btn" name="login_btn" value="Connecter" />
-                        <div style="visibility: visible">
-                            <a href="inscription.php">s'inscrire</a>
-                        </div>
-                    </form>
-                <?php } ?>
-
-
-            </div>
             <div id="defillement">
                 <marquee  direction="down" scrollamount="4" onMouseOver="this.stop();" onMouseOut="this.start();">
                     <ul>
