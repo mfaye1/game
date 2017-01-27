41cccbf076586fb8466f3687fd09ed65b2add2e6
Mamadou Faye <mamadou.faye1@yahoo.fr>
2017-01-27 14:29:35 -0500

    Merge branch 'master' of https://github.com/mfaye1/game
    # Conflicts:
    #	contact.php

--- acceuil.php
+++ acceuil.php
@@ -6,10 +6,46 @@ $page_title = 'Accueil';
 <?php
 require_once 'views/top.php';
 require_once 'views/header.php';
+require_once 'user_authenticate.php'; // Code pour authentification
+define('PS_USERNAME', 'ps_username');
+
+session_start(); // Démarrage session
+
+function is_logged_in() {
+    return (array_key_exists(PS_USERNAME, $_SESSION) && (! empty($_SESSION[PS_USERNAME]))); // Par convention, l'utilisateur est connecté si la données de session username n'est pas vide
+}
+
+?>
+<?php
+$username='';
+$password='';
+$username_valide = true;
+$password_valide = true;
+if (is_logged_in() && array_key_exists('logout_btn', $_POST)) {
+    // Déconnexion à effectuer
+    $_SESSION[PS_USERNAME] = ''; // Username vide signifie non connecté
+} else if ( ! is_logged_in()
+    && array_key_exists('login_btn', $_POST)
+    && array_key_exists('username', $_POST)
+    && array_key_exists('password', $_POST)) {
+    // Connexion à effectuer ici
+    // Filtrage et Validation
+    $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
+    $username_valide = (1===preg_match('/\w{5,}/', $username));
+    $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
+    $password_valide = (1===preg_match('/\w{6,}/', $password));
+    if ($username_valide
+        && $password_valide
+        && user_authenticate($username, $password)) {
+        // Si tout se passe bien on le connecte
+        $_SESSION[PS_USERNAME] = $username;
+    }
+}
+
 ?>
 
 <?php
-$en_reception = array_key_exists('saisie_username', $_POST) && array_key_exists('saisie_password', $_POST);
+/*$en_reception = array_key_exists('saisie_username', $_POST) && array_key_exists('saisie_password', $_POST);
 
 
 //Reception du username
@@ -32,7 +68,7 @@ if ($en_reception && $username_valide && $password_valide)
 
 //    header('Location:')
 
-?>
+*/?>
 
 <body>
 
@@ -128,35 +164,43 @@ if ($en_reception && $username_valide && $password_valide)
         <div class="col-4" id="aside">
 
             <div id="connexion">
-                <h2>CONNEXION</h2>
-                <form id="inscription" action="<?= basename(__FILE__) ?>" method="post">
 
-                    <div class="<?= $username_valide ? '' : 'invalid' ?>">
-                        <label for="saisie_username">Pseudo : </label>
-                        <input type="text" placeholder="entrez votre username" id="saisie_username"
-                               name="saisie_username"
-                               value="<?= $username ?>"/>
-                        <?php if (!$username_valide) { ?>
-                            <p>Le pseudo ou le mot de passe est incorrect</p>
+                <?php if (is_logged_in()) { ?>
+                    <h2 style="visibility: hidden">CONNEXION</h2>
+                    <marquee direction="left" scrollamount="4" onMouseOver="this.stop();" onMouseOut="this.start();">
+                        <span>Salut <?= $_SESSION[PS_USERNAME] ?> !, bienvenu a votre espace personel</span>
+                    </marquee>
+                    <form name="logout" method="post">
+                        <input type="submit" id="logout_btn" name="logout_btn" value="Déconnecter" />
+                        <div style="visibility: hidden">
+                            <a href="inscription.php">s'inscrire</a>
+                        </div>
+                    </form>
+                <?php } else { ?>
+                    <h2 style="visibility: visible">CONNEXION</h2>
+                    <form name="login" method="post">
+                        <div>
+                        <label for="username">Identifiant:</label>
+                        <input type="text" name="username" id="username"
+                               value="<?= array_key_exists('username',$_POST) ? $_POST['username'] : '' ?>"
+                        />
+                        </div>
+                        <div>
+                        <label for="password">Mot de passe:</label>
+                        <input type="password" name="password" id="password" value="" />
+                        </div>
+                        <?php // Afficher un message dans le cas ou l'authentification n'a pas réussi
+                        if (array_key_exists('login_btn',$_POST)) { ?>
+                            <p>Le pseudo et le mot de passe fournis ne concordent pas.</p>
                         <?php } ?>
-                    </div>
-                    <div class="<?= $password_valide ? '' : 'invalid' ?>">
-                        <label for="saisie_password">Mod de passe : </label>
-                        <input type="password" placeholder="entrez votre password" id="saisie_password"
-                               name="saisie_password"
-                               value="<?= $password ?>"/>
-                        <?php if (!$password_valide) { ?>
-                            <p>Le pseudo ou le mot de passe est incorrect</p>
-                        <?php } ?>
-                    </div>
-                    <div>
-                        <label></label>
-                        <input type="submit" value="connexion"/>
-                    </div>
-                </form>
-                <div>
-                    <a href="inscription.php">s'inscrire</a>
-                </div>
+                        <input type="submit" id="login_btn" name="login_btn" value="Connecter" />
+                        <div style="visibility: visible">
+                            <a href="inscription.php">s'inscrire</a>
+                        </div>
+                    </form>
+                <?php } ?>
+
+
             </div>
             <div id="defillement">
                 <marquee  direction="down" scrollamount="4" onMouseOver="this.stop();" onMouseOut="this.start();">
--- contact.php
+++ contact.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 require_once 'define.php';
 $page_title = 'contact';
@@ -7,8 +6,12 @@ $page_title = 'contact';
 require_once 'views/top.php';
 require_once 'views/header.php';
 ?>
-<div id="wrapper">
+<p id="wrapper">
+    <p>Vous pouvez nous contacter a partir des adresses suivantes ou bien nous envoyer un  commentaire aussi</p>
+    <form>
 
+        <input type="text" id="objet" name="objet" >
+    </form>
     <img src="images/le_map.png">
 </div>
 
