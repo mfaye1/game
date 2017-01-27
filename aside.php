9a63ae5be7ca871ed70e42b861d2fba32cd7edf3
mamadou faye <mamadou.faye1@yahoo.fr>
2017-01-27 10:51:03 -0500

    Add files via upload

---
+++ aside.php
@@ -0,0 +1,45 @@
+<aside>
+    <ul id="menu-deroulant">
+        <li><a href="#">Type de jeu</a>
+            <ul>
+                <li><a href="#">PS3</a></li>
+                <li><a href="#">PS4</a></li>
+                <li><a href="#">PSP</a></li>
+                <li><a href="#">PS VITA / PSP</a></li>
+                <li><a href="#">PC</a></li>
+                <li><a href="#">Xbox 360</a></li>
+                <li><a href="#">NINTENDO / DS</a></li>
+            </ul>
+        </li><!--
+ --><li><a href="#">Categorie</a>
+            <ul>
+                <li><a href="#">ARCADE</a></li>
+                <li><a href="#">ACTION</a></li>
+                <li><a href="#">AVENTURE</a></li>
+                <li><a href="#">SPORT</a></li>
+                <li><a href="#">COMBAT</a></li>
+                <li><a href="#">COURSE</a></li>
+            </ul>
+        </li><!--
+ --><li><a href="#">NOUVEAUTES</a>
+            <ul>
+                <li><a href="#">DOUBLE DRAGON 4</a></li>
+                <li><a href="#">SNIPER GHOST</a></li>
+                <li><a href="#">RESIDENT EVIL 7</a></li>
+                <li><a href="#">NFS </a></li>
+                <li><a href="#">FIFA 17</a></li>
+            </ul>
+        </li><!--
+ --><li><a href="#">POUR ENFANT</a>
+            <ul>
+                <li><a href="#">SPIDER-MAN</a></li>
+                <li><a href="#">SONIC</a></li>
+                <li><a href="#">ANGRY-BIRD</a></li>
+                <li><a href="#">ZELDA</a></li>
+            </ul>
+        </li>
+    </ul>
+    <marquee direction="down" scrollamount="2" onMouseOver="this.stop();" onMouseOut="this.start();">
+        Mon texte défile doucement
+    </marquee
+</aside>
---
+++ menu.php
@@ -0,0 +1,7 @@
+<div>
+    <ul class="row">
+        <li class="<?php if ($page_title == "Accueil") {echo "active";} ?> col-4 "><a href="acceuil.php">acceuil</a></li>
+        <li class="<?php if ($page_title == "Contact") {echo "active";} ?> col-4"><a href="contact.php">contact</a></li>
+        <li class="<?php if ($page_title == "Catalogue") {echo "active";} ?> col-4"><a href="catalogue.php">catalogue</a></li>
+    </ul>
+</div>
