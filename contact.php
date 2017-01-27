bf5828b654caa7d71154d7e163b3a3d71cba9e73
Mamadou Faye <mamadou.faye1@yahoo.fr>
2017-01-27 11:17:47 -0500

    arrangement du git

--- contact.php
+++ contact.php
@@ -11,17 +11,4 @@ require_once 'views/header.php';
 
     <img src="images/le_map.png">
 </div>
-=======
-<?php
-require_once 'define.php';
-$page_title = 'contact';
-?>
-<?php
-require_once 'views/top.php';
-require_once 'views/header.php';
-?>
-<div id="wrapper">
 
-    <img src="images/le_map.png">
-</div>
->>>>>>> 9a63ae5be7ca871ed70e42b861d2fba32cd7edf3
