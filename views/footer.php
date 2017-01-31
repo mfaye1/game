6abd138465e12d09b20c1ee5e905066f8dbe06c9
Mamadou Faye <mamadou.faye1@yahoo.fr>
2017-01-30 22:20:04 -0500

    le footer

--- views/footer.php
+++ views/footer.php
@@ -5,4 +5,41 @@
             <li class="<?php if ($page_title == "Contact") {echo "active";} ?> col-4"><a href="contact.php">contact</a></li>
             <li class="<?php if ($page_title == "Catalogue") {echo "active";} ?> col-4"><a href="catalogue.php">catalogue</a></li>
         </ul>
+    <div class="row">
+    <div class="col-3">
+        <h1>Nos boutiques</h1>
+        <ul>
+            <li><a href="#">Sony</a> </li>
+            <li><a href="#">Best-Buy</a> </li>
+            <li><a href="#">Walmart</a> </li>
+            <li><a href="#">Game-center</a> </li>
+        </ul>
+    </div>
+    <div class="col-3">
+        <h1>Services</h1>
+        <ul>
+            <li><a href="#">Playstation-video</a> </li>
+            <li><a href="#">Playstation-music</a> </li>
+            <li><a href="#">Playstation-jeu</a> </li>
+        </ul>
+    </div>
+    <div class="col-3">
+        <h1>Conditions d'utilisation</h1>
+        <ul>
+            <li><a href="#">condition d'achat</a> </li>
+            <li><a href="#">condition sur les remise </a> </li>
+            <li><a href="#">condition des licences</a> </li>
+        </ul>
+    </div>
+    <div class="col-3">
+        <h1>Reseaux Sociaux</h1>
+        <ul>
+            <li><a href="#">Blog</a> </li>
+            <li><a href="#">Support</a></li>
+        </ul>
+        <a href="facebook.com"> <img src="images/Logo-facebook-footer.png"></a>
+        <a href="twitter.com"> <img src="images/twitter-ios-logo%20copy.png"></a>
+        <a href="linkeedin.com"> <img src="images/linkeed.png"></a>
+    </div>
+    </div>
 </footer>
